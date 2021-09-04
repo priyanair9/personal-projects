cd029f477cdbe50478e44e5214dee2a5f6844c21
priyanair9 <priya.nair@students.mq.edu.au>
2021-09-04 14:11:30 +1000

    count9 method
    added a recurisive method that counts the number of 9s in an integer

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int result = count9(95991299);
+        System.out.println(result);
+    }
+
+    // method to count how many 9s are in a number
+    public static int count9(int n) {
+        int count = 0;
+        if(n < 9) {
+            return count;
+        }
+        if(n%10 == 9 || n == 9) {
+            count++;
+        }
+        return count + count9(n/10);
     }
 }
