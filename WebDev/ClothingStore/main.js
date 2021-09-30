91da86c92da0f79a576ef3af9fd6c0d7bab569d4
priyanair9 <priya.nair@students.mq.edu.au>
2021-09-30 22:49:38 +1000

    Added button functions

--- WebDev/ClothingStore/main.js
+++ WebDev/ClothingStore/main.js
@@ -1,2 +1,19 @@
 // alert("'Hello World!'");
-const menuLinks = document.querySelector('.nav-links')
+// const menuLinks = document.querySelector('.nav-links')
+
+let img = document.querySelector('#image');
+let greenButton = document.querySelector('#green');
+let redButton = document.querySelector('#red');
+let blueButton = document.querySelector('#blue');
+
+greenButton.addEventListener('click', () => {
+    img.src = 'images/Screen Shot 2021-07-09 at 5.25.46 pm.png';
+})
+
+redButton.addEventListener('click', () => {
+    img.src = 'images/Screen Shot 2021-07-09 at 5.30.12 pm.png';
+})
+
+blueButton.addEventListener('click', () => {
+    img.src = 'images/Screen Shot 2021-07-09 at 5.31.37 pm.png';
+})
