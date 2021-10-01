9d176a9139e3429404a5141b8b5db73489c4f28b
priyanair9 <priya.nair@students.mq.edu.au>
2021-10-01 23:09:21 +1000

    tried to add button state

--- WebDev/ClothingStore/main.js
+++ WebDev/ClothingStore/main.js
@@ -14,3 +14,16 @@ redButton.addEventListener('click', () => {
 blueButton.addEventListener('click', () => {
     img.src = 'images/Screen Shot 2021-07-09 at 5.31.37 pm.png';
 })
+
+if(img.src == 'images/Screen Shot 2021-07-09 at 5.30.12 pm.png') {
+    redButton.style.borderColor = '#000000';
+    document.getElementById('#red').style.borderColor = "black";
+}
+
+if(img.src == 'images/Screen Shot 2021-07-09 at 5.25.46 pm.png') {
+    greenButton.style.borderColor = '#000000';
+}
+
+if(img.src == 'images/Screen Shot 2021-07-09 at 5.31.37 pm.png') {
+    blueButton.style.borderColor = '#000000';
+}
