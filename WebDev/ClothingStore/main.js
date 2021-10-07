9889ccab6c900d8a1933da7219b3d92a0864987f
priyanair9 <priya.nair@students.mq.edu.au>
2021-10-07 20:58:42 +1100

    Added colour selection option

--- WebDev/ClothingStore/main.js
+++ WebDev/ClothingStore/main.js
@@ -5,25 +5,21 @@ let blueButton = document.querySelector('#blue');
 
 greenButton.addEventListener('click', () => {
     img.src = 'images/Screen Shot 2021-07-09 at 5.25.46 pm.png';
+    greenButton.style.borderColor = '#000000';
+    redButton.style.borderColor = '#f46a6a';
+    blueButton.style.borderColor = '#c7dbfa';
 })
 
 redButton.addEventListener('click', () => {
     img.src = 'images/Screen Shot 2021-07-09 at 5.30.12 pm.png';
+    redButton.style.borderColor = '#000000';
+    greenButton.style.borderColor = '#5ea089';
+    blueButton.style.borderColor = '#c7dbfa';
 })
 
 blueButton.addEventListener('click', () => {
     img.src = 'images/Screen Shot 2021-07-09 at 5.31.37 pm.png';
-})
-
-if(img.src == 'images/Screen Shot 2021-07-09 at 5.30.12 pm.png') {
-    redButton.style.borderColor = '#000000';
-    document.getElementById('#red').style.borderColor = "black";
-}
-
-if(img.src == 'images/Screen Shot 2021-07-09 at 5.25.46 pm.png') {
-    greenButton.style.borderColor = '#000000';
-}
-
-if(img.src == 'images/Screen Shot 2021-07-09 at 5.31.37 pm.png') {
     blueButton.style.borderColor = '#000000';
-}
+    redButton.style.borderColor = '#f46a6a';
+    greenButton.style.borderColor = '#5ea089';
+})
